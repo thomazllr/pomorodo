bf95113dbd43e9cc3ae7a72ce994ab940895b48a
thomazllr <guithomaz270@gmail.com>
2025-05-22 17:05:44 -0300

    feat: add lucide-react dependency and integrate TimerIcon in Heading component

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,17 @@ import './styles/theme.css';
 import './styles/global.css';
 
 import { Heading } from './components/Heading';
+import { TimerIcon } from 'lucide-react';
 
 export function App() {
   return (
     <>
-      <Heading />
+      <Heading>
+        Olá
+        <button title='Timer'>
+          <TimerIcon />
+        </button>
+      </Heading>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque
         exercitationem enim voluptates recusandae unde dicta reprehenderit et
--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,5 +1,11 @@
 import styles from './Heading.module.css';
 
-export function Heading() {
-  return <h1 className={styles.heading}>Hello World</h1>;
+// Desestrutração do Javascript, como eu sei que eu recebo um props eu posso pegar a variável que eu quero direto.
+
+type HeadingProps = {
+  children: React.ReactNode;
+};
+
+export function Heading({ children }: HeadingProps) {
+  return <h1 className={styles.heading}>{children}</h1>;
 }
