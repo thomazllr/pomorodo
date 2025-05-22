289e25a3d3bae93a029c5034569228bf7f9127ab
thomazllr <guithomaz270@gmail.com>
2025-05-22 15:50:57 -0300

    add: aprendendo sobre css modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-function App() {
+import './styles/theme.css';
+import './styles/global.css';
+
+import { Heading } from './components/Heading';
+
+export function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <>
+      <Heading />
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque
         exercitationem enim voluptates recusandae unde dicta reprehenderit et
         ratione, odio saepe dolor illum impedit labore vero temporibus aliquam
         rem nemo. Culpa!
       </p>
-    </div>
+    </>
   );
 }
-
-export default App;
