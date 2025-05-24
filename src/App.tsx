fad1719dc6f10ca3968d3ad116d5c17c9fde450b
thomazllr <guithomaz270@gmail.com>
2025-05-24 04:59:54 -0300

    feat: add CountDown component and integrate form in App, update global styles

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Logo } from './components/Logo';
 import './styles/theme.css';
 import './styles/global.css';
 import { Menu } from './components/Menu';
+import { CountDown } from './components/CountDown';
 
 export function App() {
   return (
@@ -17,11 +18,29 @@ export function App() {
       </Container>
 
       <Container>
-        <section>FORM</section>
+        <CountDown />
       </Container>
 
       <Container>
-        <section>FOOTER</section>
+        <form className='form' action=''>
+          <div className='formRow'>
+            <label htmlFor='input'>task: </label>
+            <input id='input' type='text' />
+          </div>
+
+          <div className='formRow'>
+            <span>Lorem ipsum dolor sit amet</span>
+          </div>
+
+          <div className='formRow'>
+            <span>Ciclos</span>
+            <span>0 0 0 0 0</span>
+          </div>
+
+          <div className='formRow'>
+            <button>Enviar</button>
+          </div>
+        </form>
       </Container>
     </>
   );
